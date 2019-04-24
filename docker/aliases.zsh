8ac0a0b94f25698033ced18de08473bed5bb95a1
Derek Kraan <derek.kraan@gmail.com>
2019-04-24 13:31:16 +0200

    UPDATES

---
+++ docker/aliases.zsh
@@ -0,0 +1,2 @@
+alias dc='docker-compose up'
+alias dl='docker-compose logs -f --tail=20'
--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,13 +1,3 @@
-alias r='rbenv local 2.0.0-p195'
-
-alias sc='script/console'
-alias sg='script/generate'
-alias sd='script/destroy'
-
-alias migrate='rake db:migrate db:test:clone'
-
 alias be='bundle exec'
 
 alias cl='for file in $(find . -regex ".*\.log"); do echo > $file; done'
-
-alias pspec='DISABLE_SPRING=1 rake spec'
