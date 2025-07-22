5c20c4a651af4ef8b55b793eefd4ced27bea3886
Derek Kraan <derek@kraan.dev>
2025-07-22 08:26:35 +0200

    Fix tmux init

--- tmux/init_7sage.sh
+++ tmux/init_7sage.sh
@@ -27,7 +27,7 @@ tmux send-keys -t "$SESSION:phx_server" "cd ~/Code/7sage/phoenix" C-m "clear" C-
 tmux new-window -t $SESSION:2 -n "port_fw"
 tmux send-keys -t "$SESSION:port_fw" "cd ~/Code/7sage/phoenix" C-m "clear" C-m "just port-forward" C-m
 
-tmux new-window -t $SESSION:2 -n "lazygit"
+tmux new-window -t $SESSION:3 -n "lazygit"
 tmux send-keys -t "$SESSION:lazygit" "cd ~/Code/7sage/phoenix" C-m "clear" C-m "lazygit" C-m
 
 att
